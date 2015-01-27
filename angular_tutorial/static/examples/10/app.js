34852be1e3f1ccfbc4aaade7157ee7e51e642336
Björn Ricks <bjoern.ricks@intevation.de>
2015-01-27 17:19:25 +0100

    Add example for separated code

---
+++ angular_tutorial/static/examples/10/app.js
@@ -0,0 +1,15 @@
+var nameApp = angular.module('nameApp', []);
+
+nameApp.controller('NameCtrl', function($scope, $timeout) {
+    $scope.firstName = 'John';
+    $scope.lastName = 'Doe';
+
+    $scope.$watch('lastName', function(newValue, oldValue) {
+        console.log('new value is ' + newValue);
+    });
+
+    $timeout(function() {
+        $scope.lastName = 'Smith';
+    }, 1000);
+});
+
