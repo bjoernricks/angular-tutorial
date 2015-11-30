46231ba1c023016e5123f4d84100efc1989e05e2
Björn Ricks <bjoern.ricks@gmail.com>
2015-11-30 11:37:37 +0100

    Update coding style
    Use style advices from John Papa https://github.com/johnpapa/angular-styleguide

--- angular_tutorial/js/app.js
+++ angular_tutorial/js/app.js
@@ -5,36 +5,49 @@
 (function() {
     'use strict';
 
-    var angTutApp = angular.module('angTutApp', ['ngRoute', 'ngMaterial']);
+    angular.module('angTutApp',
+        ['ngRoute', 'ngMaterial']
+    )
+    .config(app_config)
+    .factory('conf', conf)
+    .factory('examples', examples)
+    .factory('menu', menu)
+    .directive('atHljs', at_hljs)
+    .controller('HomeCtrl', home_ctrl)
+    .controller('SectionCtrl', section_ctrl)
+    .controller('ExampleDetailCtrl', example_detail_ctrl);
 
-    angTutApp.config(['$routeProvider',
-        function($routeProvider) {
-            $routeProvider.
-                when('/', {
-                    templateUrl: 'home.html'
-                }).
-                when('/examples/:exampleNumber', {
-                    templateUrl: 'example-view.html',
-                    controller: 'ExampleDetailCtrl'
-                }).
-                when('/sections/:sectionNumber', {
-                    templateUrl: 'section-view.html',
-                    controller: 'SectionCtrl'
-                }).
-                otherwise({
-                    redirectTo: '/'
-                });
-        }]
-    );
+    app_config.$inject = ['$routeProvider'];
 
-    angTutApp.factory('conf', function() {
+    function app_config($routeProvider) {
+        $routeProvider.
+        when('/', {
+                templateUrl: 'home.html'
+        }).
+        when('/examples/:exampleNumber', {
+                templateUrl: 'example-view.html',
+                controller: 'ExampleDetailCtrl'
+        }).
+        when('/sections/:sectionNumber', {
+                templateUrl: 'section-view.html',
+                controller: 'SectionCtrl'
+        }).
+        otherwise({
+                redirectTo: '/'
+        });
+    }
+
+    function conf() {
         var service = {
             examplesPath: 'examples/',
         };
         return service;
-    });
+    }
 
-    angTutApp.factory('examples', function($http) {
+
+    examples.$inject = ['$http'];
+
+    function examples($http) {
         function getData(callback) {
             $http({
                 method: 'GET',
@@ -67,10 +80,11 @@
                 });
             }
         };
-    });
+    }
 
-    angTutApp.factory('menu', function() {
-        return self = {
+
+    function menu() {
+        return {
             selectSection: function(section) {
                 self.openedSection = section;
             },
@@ -88,39 +102,47 @@
                 return self.openedSection.name;
             }
         };
-    });
+    }
 
-    angTutApp.controller('HomeCtrl', function($scope, examples, menu) {
+
+    home_ctrl.$inject = ['$scope', 'examples', 'menu'];
+
+    function home_ctrl($scope, examples, menu) {
         $scope.menu = menu;
         examples.list(function(examples) {
             $scope.examples = examples;
         });
-    });
+    }
 
-    angTutApp.controller('ExampleDetailCtrl', ['$scope', '$routeParams', '$http', '$sce', 'conf',
-            'examples', 'menu',
-        function($scope, $routeParams, $http, $sce, conf, examples, menu) {
-            $scope.menu = menu;
-            examples.find($routeParams.exampleNumber, function(example) {
-                $scope.example = example;
-                var examplePath = conf.examplesPath + example.id + '/';
-                $scope.runUrl = examplePath + 'index.html';
-                $scope.readme = examplePath + 'readme.html';
-            });
-    }]);
 
-    angTutApp.controller('SectionCtrl', ['$scope', '$routeParams', '$http', '$sce', 'conf', 'examples',
-            'menu',
-        function($scope, $routeParams, $http, $sce, conf, examples, menu) {
-            $scope.menu = menu;
-            examples.findSection($routeParams.sectionNumber, function(section) {
-                $scope.section = section;
-                $scope.readme = conf.examplesPath + 'section' + section.id + '.html';
-            });
-        }
-    ]);
+    example_detail_ctrl.$inject = ['$scope', '$routeParams', '$http', '$sce', 'conf',
+        'examples', 'menu'];
 
-    angTutApp.directive('atHljs', ['conf', function(conf) {
+    function example_detail_ctrl($scope, $routeParams, $http, $sce, conf, examples, menu) {
+        $scope.menu = menu;
+        examples.find($routeParams.exampleNumber, function(example) {
+            $scope.example = example;
+            var examplePath = conf.examplesPath + example.id + '/';
+            $scope.runUrl = examplePath + 'index.html';
+            $scope.readme = examplePath + 'readme.html';
+        });
+    }
+
+
+    section_ctrl = ['$scope', '$routeParams', '$http', '$sce', 'conf', 'examples', 'menu'];
+
+    function section_ctrl($scope, $routeParams, $http, $sce, conf, examples, menu) {
+        $scope.menu = menu;
+        examples.findSection($routeParams.sectionNumber, function(section) {
+            $scope.section = section;
+            $scope.readme = conf.examplesPath + 'section' + section.id + '.html';
+        });
+    }
+
+
+    at_hljs.$inject = ['conf'];
+
+    function at_hljs(conf) {
         return {
             scope: { file: '=', example: '=' },
             restrict: 'A',
@@ -156,5 +178,5 @@
                 });
             }
         };
-    }]);
+    }
 })();
