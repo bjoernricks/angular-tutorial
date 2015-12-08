2dfb6d6b19335d241bcc029d85aab0a39f604654
Björn Ricks <bjoern.ricks@gmail.com>
2015-12-08 17:43:40 +0100

    Add routing example with angular ui router

---
+++ angular_tutorial/examples/33/app.js
@@ -0,0 +1,54 @@
+angular.module('nameApp',
+    [
+        'ui.router',
+        'nameApp.controllers'
+    ])
+    .factory('PersonsService', personsService)
+    .config(configRoutes);
+
+personsService.$inject = ['$http'];
+
+function personsService($http) {
+    var service = {
+        getAll: get_all,
+        get: get,
+    };
+    return service;
+
+    function get_all() {
+        return $http.get('/examples/32/names.json').then(function(response) {
+            return response.data;
+        });
+    }
+
+    function get(id) {
+        return $http.get('/examples/32/names.json').then(function(response) {
+            for(var i=0; i < response.data.length; i++) {
+                var data = response.data[i];
+                if (data.id == id) {
+                    return data;
+                }
+            }
+        });
+    }
+}
+
+configRoutes.$inject = ['$urlRouterProvider', '$stateProvider'];
+
+function configRoutes($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('list', {
+                url: '/',
+                templateUrl: 'list.html',
+                controller: 'ExampleCtrl',
+                controllerAs: 'ctrl',
+        })
+        .state('detail', {
+                url: '/:id',
+                templateUrl: 'detail.html',
+                controller: 'ExampleDetailCtrl',
+                controllerAs: 'ctrl',
+        });
+}
